c862246e11775c7526bd465fe5693f6a803d34fa
David Kinzer <dtkinzer@gmail.com>
2015-01-24 19:11:18 -0500

    Use beni for syntax highlighting.

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
-import "os"
-import "bufio"
+import (
+  "os"
+  "bufio"
+  "github.com/koron/beni"
+)
 
 func main() {
   stdin := bufio.NewReader(os.Stdin)
   stdout := bufio.NewWriter(os.Stdout)
-
   defer stdout.Flush()
 
-  for {
-    line, _, err := stdin.ReadLine()
-
-    if err != nil {
-      break
-    }
-
-    stdout.Write(append(line, '\n'))
-  }
+  beni.Highlight(stdin, stdout, "go", "base16", "Terminal256")
 }
