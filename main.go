1b307b0839461e719729f0b1da7d1143eef688b2
David Kinzer <dtkinzer@gmail.com>
2015-01-24 19:14:53 -0500

    Go format.

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "os"
-  "bufio"
-  "github.com/koron/beni"
+	"bufio"
+	"github.com/koron/beni"
+	"os"
 )
 
 func main() {
-  stdin := bufio.NewReader(os.Stdin)
-  stdout := bufio.NewWriter(os.Stdout)
-  defer stdout.Flush()
+	stdin := bufio.NewReader(os.Stdin)
+	stdout := bufio.NewWriter(os.Stdout)
+	defer stdout.Flush()
 
-  beni.Highlight(stdin, stdout, "go", "base16", "Terminal256")
+	beni.Highlight(stdin, stdout, "go", "base16", "Terminal256")
 }
